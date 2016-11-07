233c8a36d647f8c6a9c265aa75e47c9dc3fbf25f
Alexander Gorban <gorban.alexander.y@gmail.com>
2016-11-07 19:08:35 +0200

    WebAPI 2. Patients and Data (Add MongoDb, and base class)

---
+++ PatientData/App_Start/MongoConfig.cs
@@ -0,0 +1,42 @@
+﻿using MongoDB.Driver.Linq;
+using PatientData.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PatientData
+{
+    public class MongoConfig
+    {
+        public static void Seed()
+        {
+            var patients = PatientDb.Open();
+
+            if (!patients.AsQueryable().Any(p => p.Name == "Scott"))
+            {
+                var data = new List<Patient>()
+                {
+                    new Patient
+                    {
+                        Name = "Scott",
+                        Ailments = new List<Ailment>() { new Ailment { Name = "Crazy"} },
+                        Medications = new List<Medication> { new Medication { Name = "B6", Doses = 100} }
+                    },
+                    new Patient
+                    {
+                        Name = "Joy",
+                        Ailments = new List<Ailment>() { new Ailment { Name = "Crazy"} },
+                        Medications = new List<Medication> { new Medication { Name = "Complex B", Doses = 500 } }
+                    },
+                    new Patient
+                    {
+                        Name = "Sarah",
+                        Ailments = new List<Ailment>() { new Ailment { Name = "Crazy"} },
+                        Medications = new List<Medication> { new Medication { Name = "Complex vitamin", Doses = 1000 } }
+                    }
+                };
+
+                patients.InsertBatch(data);
+            }
+        }
+    }
+}
---
+++ PatientData/Models/PatientDb.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Driver;
+
+namespace PatientData.Models
+{
+    public class PatientDb
+    {
+        public static MongoCollection<Patient> Open()
+        {
+            var client = new MongoClient("mongodb://localhost:27017/PatientData");
+            var server = client.GetServer();
+            var db = server.GetDatabase("PatientDb");
+            return db.GetCollection<Patient>("Patients");
+        }
+    }
+}
